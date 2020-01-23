880db284b968347ad0e6ff7d4f7905719b7f7f80
fatimanehsan <fatimanoorehsan99@gmail.com>
2020-01-23 12:14:13 +0100

    Person klasse

---
+++ src/main/java/Person.java
@@ -0,0 +1,11 @@
+public class Person {
+    private String navn;
+    private int alder;
+    private int fodselsdato;
+    private String epost;
+    private String telefonnummer;
+
+    public Person(String navn) {
+        this.navn = navn;
+    }
+}
