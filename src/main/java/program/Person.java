83e9ea9b2b8cc4e33fd474361bad1bbc3ab8fc84
OleLøc <olebastiank.lochen@gmail.com>
2020-01-23 12:49:17 +0100

    scene

--- src/main/java/program/Person.java
+++ src/main/java/program/Person.java
@@ -7,7 +7,11 @@ public class Person {
     private String epost;
     private String telefonnummer;
 
-    public Person(String navn) {
+    public Person(String navn, int alder, int fodselsdato, String epost, String telefonnummer) {
         this.navn = navn;
+        this.alder = alder;
+        this.fodselsdato = fodselsdato;
+        this.epost = epost;
+        this.telefonnummer = telefonnummer;
     }
 }
