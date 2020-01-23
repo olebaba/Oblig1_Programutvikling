4920f66c8dcea27d957457d77b9de60528bb231e
OleLøc <olebastiank.lochen@gmail.com>
2020-01-23 11:48:42 +0100

    ting

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -1,5 +1,10 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MainApp extends Application {
 
@@ -8,7 +13,14 @@ public class MainApp extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("MainScene.fxml"));
 
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
+
+        primaryStage.setTitle("JavaFX and Maven");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
