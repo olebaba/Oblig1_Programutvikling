c6d239491bdc2c1c60be2821e81eca8496be50a6
Zandraa <zandra.max@gmail.com>
2020-02-12 16:27:52 +0100

    menubar med filvalg

--- src/main/java/program/Person.java
+++ src/main/java/program/Person.java
@@ -1,5 +1,7 @@
 package program;
 
+import javafx.fxml.FXML;
+
 public class Person {
     private String navn;
     private int alder;
--- src/main/java/program/controllers/MainAppController.java
+++ src/main/java/program/controllers/MainAppController.java
@@ -1,15 +1,38 @@
 package program.controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
+
+import java.io.File;
 
 public class MainAppController {
 
-    @FXML
+   /* @FXML
     private Label label;
 
     public void initialize() {
         // TODO
         label.setText("heiiii");
+    }*/
+    @FXML
+    private MenuItem apneFil;
+    @FXML
+    private MenuItem lagreFil;
+
+    public void TrykketLagreFil(ActionEvent actionEvent) {
+
+
+    }
+
+
+    public void TrykketApneFil(ActionEvent actionEvent) {
+        FileChooser filValg = new FileChooser();
+        filValg.getExtensionFilters().add(new ExtensionFilter("Text files", "*.txt"));
+        filValg.showOpenDialog(null);
     }
 }
