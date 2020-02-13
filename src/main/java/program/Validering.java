ff32b00dccee1c8cfd4bded491a6fb3917e3866a
OleLøc <olebastiank.lochen@gmail.com>
2020-02-13 10:27:53 +0100

    validering og validertest ferdig

--- src/main/java/program/Validering.java
+++ src/main/java/program/Validering.java
@@ -1,22 +1,38 @@
 package program;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.time.Year;
 
 public class Validering {
 
     public static boolean validerAlder(int alder){
-        if(alder<0 || alder>120){
-            return false;
-        }
-        else{
-            return true;
-        }
+        return (alder > 0 && alder < 120);
     }
 
     public static boolean validerNavn(String navn){
         return ((navn != null)
                 && (!navn.equals(""))
                 && (navn.matches("^[a-zæøåA-ZÆØÅ ]*$")));
+    }
+
+    public static boolean validerFodselsmnd(int mnd){
+        return (mnd > 0 && mnd < 13);
+    }
+
+    public static boolean validerFodselsdag(int dag){
+        return (dag > 0 && dag < 32);
+    }
+
+    public static boolean validerFodselsar(int ar){
+        return (ar > 1900 && ar < Integer.parseInt(String.valueOf(Year.now())));
+    }
+
+    public static boolean validerEpost(String epost){
+        return ((epost != null)
+                && (!epost.equals(""))
+                && (epost.matches("([a-zA-Z0-9_\\-\\.]+)@([a-zA-Z0-9_\\-\\.]+)\\.([a-zA-Z]{2,5})")));
+    }
+
+    public static boolean validerTlf(String tlf){
+        return ((tlf != null) && (!tlf.equals("")) && (tlf.matches("(\\d{8})")));
     }
 }
--- src/main/java/program/ValideringTest.java
+++ src/main/java/program/ValideringTest.java
@@ -1,15 +1,48 @@
 package program;
 
+import org.junit.jupiter.api.Test;
+import program.Exceptions.InvalidAgeException;
+
 class ValideringTest {
+    PersonRegister person = new PersonRegister("ole", 20, 2, 27, 1999,
+            "minmail@main.com", "98765432");
+
+    ValideringTest() throws InvalidAgeException {
+    }
 
     @org.junit.jupiter.api.Test
-    void validerAlder(String name) {
+    void validerAlder() {
+        if(!Validering.validerAlder(person.getAlder())) System.out.println("Feil alder!");
     }
 
     @org.junit.jupiter.api.Test
     void validerNavn() {
-        String name = "Ole Bastian Løchen";
-        if(Validering.validerNavn(name)) System.out.println("ja");
-        else System.out.println("nei");
+        if(!Validering.validerNavn(person.getNavn())) System.out.println("Feil navn!");
+    }
+
+    @Test
+    void validerFodselsmnd() {
+        if(!Validering.validerFodselsmnd(person.getFødselsmnd())) System.out.println("Feil mnd!");
+    }
+
+    @Test
+    void validerFodselsdag() {
+        if(!Validering.validerFodselsdag(person.getFødselsdag())) System.out.println("Feil dag!");
+    }
+
+    @Test
+    void validerFodselsar() {
+        if(!Validering.validerFodselsar(person.getFødselsår())) System.out.println("Feil år!");
+    }
+
+    @Test
+    void validerEpost() {
+        if(!Validering.validerEpost(person.getEpost())) System.out.println("Feil epost!");;
+    }
+
+
+    @Test
+    void validerTlf() {
+        if(!Validering.validerTlf(person.getTelefonnummer())) System.out.println("Feil nummer!");
     }
 }
