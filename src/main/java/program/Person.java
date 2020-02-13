b76e477ec20de528f28a5ca7c421b86149082f1e
Zandraa <zandra.max@gmail.com>
2020-02-13 10:26:00 +0100

    små endringer, litt testing med serialisering

--- src/main/java/program/Person.java
+++ src/main/java/program/Person.java
@@ -3,6 +3,7 @@ package program;
 import javafx.fxml.FXML;
 
 public class Person {
+
     private String navn;
     private int alder;
     private int fodselsdato;
--- src/main/java/program/controllers/MainAppController.java
+++ src/main/java/program/controllers/MainAppController.java
@@ -7,31 +7,57 @@ import javafx.scene.control.Menu;
 import javafx.scene.control.MenuItem;
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
+import javafx.stage.Window;
 
-import java.io.File;
+import java.io.*;
 
 public class MainAppController {
-
-   /* @FXML
-    private Label label;
-
-    public void initialize() {
-        // TODO
-        label.setText("heiiii");
-    }*/
+    @FXML
+    private Label navn;
+    @FXML
+    private Label alder;
+    @FXML
+    private Label fodselsdato;
+    @FXML
+    private Label epost;
+    @FXML
+    private Label nummer;
     @FXML
     private MenuItem apneFil;
     @FXML
     private MenuItem lagreFil;
-
-    public void TrykketLagreFil(ActionEvent actionEvent) {
-
-
+    @FXML
+    public void TrykketLagreFil(ActionEvent actionEvent) throws FileNotFoundException, IOException, ClassNotFoundException {
+        File nyFil = new File("test.txt");
+        /*try{
+            PrintWriter output = new PrintWriter(nyFil);
+            output.println("Zandra Max");
+            output.println(20);
+            output.close();
+        }catch (IOException ex){
+            System.out.println("Noe gikk feil");
+        }*/
+        //Serialisering
+        String hei = "heihei";
+        FileOutputStream fileOutput = new FileOutputStream(nyFil);
+        ObjectOutputStream output = new ObjectOutputStream(fileOutput);
+        output.writeObject(hei);
+        output.close();
+        fileOutput.close();
+/*
+        //de-serialisering
+        FileInputStream fileInput = new FileInputStream(nyFil);
+        ObjectInputStream input = new ObjectInputStream(fileInput);
+        String hei2 = "heihallo";
+        input.readObject();
+*/
     }
 
-
+    @FXML
     public void TrykketApneFil(ActionEvent actionEvent) {
+        //vet ikke om dette fungerer, lurer på om vi kanskje skal bruke scanner()?
         FileChooser filValg = new FileChooser();
+        filValg.setTitle("Velg én eller flere *.txt-filer");
         filValg.getExtensionFilters().add(new ExtensionFilter("Text files", "*.txt"));
         filValg.showOpenDialog(null);
     }
