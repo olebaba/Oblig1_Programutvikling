5181c0ead5b0035a734b8e480e63f4cb4cf5289a
fatimanehsan <fatimanoorehsan99@gmail.com>
2020-02-21 15:56:32 +0100

    fikset litt på person klasse

--- src/main/java/program/PersonRegister.java
+++ src/main/java/program/PersonRegister.java
@@ -1,10 +1,10 @@
 package program;
 
-import program.Exceptions.InvalidAgeException;
+import program.Exceptions.*;
 
 public class PersonRegister extends Person{
     public PersonRegister(String navn, int alder, int fodselsmnd, int fodselsdag,
-                          int fodselsar, String epost, String telefonnummer) throws InvalidAgeException {
+                          int fodselsar, String epost, String telefonnummer) throws java.io.IOException {
         super(navn, alder, fodselsmnd, fodselsdag, fodselsar, epost, telefonnummer);
     }
 }
